5c09970b4d1ce3466b0d4018b4d6b5b439ba5b7d
Pavel23791 <140964820+Pavel23791@users.noreply.github.com>
2023-08-31 20:14:57 +0300

    add new message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from cicd
+          This is my ne message
         </p>
         <a
           className="App-link"
